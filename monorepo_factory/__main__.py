1c5911a2a73322b5aa4204d244f5deb65a666d73
Jacob Valdez <jacobfv123@gmail.com>
2023-11-26 05:17:08 -0600

    it works

--- monorepo_factory/__main__.py
+++ monorepo_factory/__main__.py
@@ -1,197 +1,175 @@
-import shutil
-from textwrap import dedent
-from typing import final
-import typer
-from rich.console import Console
-from rich.tree import Tree
-from box import Box
-from subprocess import check_output, CalledProcessError
+from contextlib import contextmanager
+from functools import reduce
 import os
+import re
+import shutil
+import subprocess
+from typing import Dict
+from box import Box
 import networkx as nx
-import fnmatch
+import typer
+from pathlib import Path
+from rich.console import Console
 
-
-app = typer.Typer()
+# Initialize Rich console
 console = Console()
 
 
-def sh(cmd: str, cwd: str = None):
+
+@contextmanager
+def cwd(path):
+    """Context manager for changing the current working directory."""
+    original_path = os.getcwd()
+
+    path = Path(path)
+    original_path = Path(original_path)
+
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
     try:
-        return check_output(cmd, shell=True, cwd=cwd, text=True)
-    except CalledProcessError as e:
+        os.chdir(str(path))
+        yield original_path, path
+    finally:
+        os.chdir(str(original_path))
+
+
+def sh(cmd: str, cwd: str = None):
+    """Execute a shell command in a specific directory and return its output."""
+    try:
+        return subprocess.check_output(cmd, shell=True, cwd=cwd, text=True)
+    except subprocess.CalledProcessError as e:
         console.print(f"An error occurred while executing the command: {cmd}")
         console.print(f"Error details: {e.output}")
         raise
 
 
-def sort_patterns(patterns):
-    # Sort patterns by their specificity (length in this case)
-    return sorted(patterns, key=lambda p: len(p["pattern"]), reverse=True)
+def identify_repos(data: Box) -> Box[str, dict]:
+    """Identify all repositories and their contents from the provided data."""
+    repos = {repo_data.name: repo_data for repo_data in data.repo}
+    for repo_data in data.repo:
+        for subrepo in repo_data.get("subrepos", []):
+            repos.setdefault(subrepo.name, {}).update({"name": subrepo.name})
+    return Box(repos)
 
 
-def apply_defaults_to_repo(repo, patterns):
-    for pattern in patterns:
-        if fnmatch.fnmatch(repo.name, pattern.get("pattern", "*")):
-            for key, value in pattern.items():
-                if key != "pattern":
-                    repo.setdefault(key, value)
-    return repo
+def identify_patterns(data: Box) -> Box[str, dict]:
+    """Identify all patterns and their contents from the provided data."""
+    return {pattern_data.pattern: pattern_data for pattern_data in data.pattern}
 
 
-def build_dependency_graph(repos):
+def prep_repos(repos: Box[str, dict], patterns: Box[str, dict]):
+    # add path
+    for repo in repos.values():
+        repo["path"] = Path(repo["name"])
+        if repo.path.exists():
+            shutil.rmtree(str(repo.path))
+    # add path to root (handles cases for both ./org/repo and ./userrepo)
+    for repo in repos.values():
+        repo["path_to_root_from_outside"] = Path("./.." if "/" in repo.name else ".")
+        repo["path_to_root_from_inside"] = repo.path_to_root_from_outside / ".."
+
+    # apply patterns to all matching repos
+    matching_patterns = {
+        repo.name: [
+            pattern_data
+            for pattern, pattern_data in patterns.items()
+            if re.match(pattern, repo.name)
+        ]
+        for repo in repos.values()
+    }
+    for repo_name in repos.keys():
+        matching_patterns_by_priority = sorted(
+            matching_patterns[repo_name],
+            key=lambda pattern: len(
+                max(re.findall(pattern.pattern, repo_name), key=len, default="")
+            ),
+        )
+        repo_data_overrides = matching_patterns_by_priority + [repos[repo_name]]
+        repos[repo_name] = reduce(lambda x, y: {**x, **y}, repo_data_overrides)
+
+def initialize_repo(repo):
+    # Initialize with README
+    with open("README.md", "w") as file:
+        file.write(f"# {repo.name}\n\n{repo.get('description', None)}")
+    sh(f"git add .")
+    sh(f'git commit -m "Initial commit"')
+
+def build_clone_graph(repos: Dict[str, dict]) -> nx.DiGraph:
+    """Build a graph representing the creation order of repositories."""
     graph = nx.DiGraph()
-
-    # Iterate through each repo and subrepo to build the graph
-    for repo in repos:
-        # Add each top-level repo as a node
-        graph.add_node(repo.name)
-
-        # If the repo has a clone source, add an edge from the clone source to the repo
-        # This indicates that the repo depends on its clone source
+    graph.add_nodes_from(repos.keys())
+    for repo in repos.values():
         if "clone" in repo:
             graph.add_edge(repo.clone, repo.name)
+    return graph
 
-        # Process subrepos if any
-        if "subrepos" in repo:
-            for subrepo in repo.subrepos:
-                # Add each subrepo as a node
-                graph.add_node(subrepo.name)
+def create_repo(repo):
+    """Create a new repository."""
+    repo.path.mkdir(parents=True, exist_ok=True)
+    with cwd(repo.path):
+        sh(f"git init")
+        initialize_repo(repo)
 
-                # If the subrepo has a clone source, add an edge from the clone source to the subrepo
-                # This ensures the subrepo is created before being added as a submodule
-                if "clone" in subrepo:
-                    graph.add_edge(subrepo.clone, subrepo.name)
 
-                # Create a dependency from the parent repo to its subrepo
-                # This ensures that the subrepos are processed before its parent
-                graph.add_edge(subrepo.name, repo.name)
+def clone_repo(repo, clone):
+    """Clone a repository from a source."""
+    with cwd(repo.path.parent):
+        clone_relpath = repo.path_to_root_from_outside / clone.path
+        sh(
+            f"git clone {clone_relpath} {repo.path.name}"
+        )
+        with cwd(repo.path.name):
+            initialize_repo(repo)
 
+
+def create_or_clone_repos(repos: Dict[str, dict]):
+    """Create or clone repositories based on the provided graph."""
+    clone_graph = build_clone_graph(repos)
+    for repo_name in nx.topological_sort(clone_graph):
+        repo = repos[repo_name]
+
+        if 'clone' in repo:
+            clone_repo(repo, repos[repo.clone])
+        else:
+            create_repo(repo)
+
+
+def build_submodule_graph(repos: Dict[str, dict]) -> nx.DiGraph:
+    """Build a graph representing the relationships between repositories and submodules."""
+    graph = nx.DiGraph()
+    for repo in repos.values():
+        for subrepo in repo.get("subrepos", []):
+            graph.add_edge(repo.name, subrepo.name)
     return graph
 
 
-def create_or_update_repo(repos, repo, base_path):
-    org_name = None
-    match repo.name.split("/"):
-        case [org_name, repo_name]:
-            org_path = os.path.join(base_path, org_name)
-            repo_path = os.path.join(org_path, repo_name)
-            if not os.path.exists(org_path):
-                os.makedirs(org_path, exist_ok=True)
-        case [repo_name]:
-            repo_path = os.path.join(base_path, repo_name)
-        case _:
-            raise ValueError(f"Invalid repository name: {repo.name}")
+def setup_submodules(repos: Dict[str, dict]):
+    """Setup submodules for the repositories based on the provided graph."""
+    submodule_graph = build_submodule_graph(repos)
+    for repo_name in nx.topological_sort(submodule_graph):
+        repo = repos[repo_name]
+        if 'subrepos' in repo and repo.subrepos:
+            with cwd(repo.path):
+                for subrepo_name in submodule_graph.successors(repo.name):
+                    subrepo_in_repo = next(subrepo for subrepo in repo.subrepos if subrepo.name == subrepo_name)
+                    sh(f"git submodule add {repo.path_to_root_from_inside / repos[subrepo_name].path} {subrepo_in_repo.path}")
 
-    if os.path.exists(repo_path):
-        shutil.rmtree(repo_path)
-
-    if "clone" in repo:
-        sh(f"git clone {repo.clone} {repo_path}")
-    else:
-        os.makedirs(repo_path, exist_ok=True)
-        sh(f"git init", cwd=repo_path)
-    with open(os.path.join(repo_path, "README.md"), "w") as readme:
-        readme.write(f"# {repo.name}")
-        if "description" in repo:
-            readme.write(f"\n\n{repo.description}")
-    sh(f"git add .", cwd=repo_path)
-    sh(f"git commit -m 'initial commit'", cwd=repo_path)
-
-    currently_created_branches = set()
-    if "submodules" in repo:
-        for subrepo_entry in repo.subrepos:
-            suprepo_name = subrepo_entry.name
-            subrepo = next((r for r in repos if r.name == repo_name), None)
-            if (
-                "branches" in repo
-                and "branches" in subrepo
-                and repo.branches
-                and subrepo.branches
-            ):
-                # add submodule for each branch that matches
-                for branch in repo.branches:
-                    sh(f"git checkout -b {branch}", cwd=repo_path)
-            else:
-                # just use the subrepo default branch
-                match subrepo.name.split("/"):
-                    case [org_name, subrepo_dirname]:
-                        relative_subrepo_path = f"../{subrepo_dirname}"
-                    case [subrepo_org_name, subrepo_dirname]:
-                        relative_subrepo_path = (
-                            f"../../{subrepo_org_name}/{subrepo_dirname}"
-                        )
-                    case [subrepo_dirname]:
-                        relative_subrepo_path = f"../{subrepo_dirname}"
-                    case _:
-                        raise Exception(f"Invalid format: {subrepo.name}")
-
-                sh(
-                    f"git submodule add {relative_subrepo_path}.git {subrepo.path}",
-                    cwd=repo_path,
-                )
-
-    if "branches" in repo:
-        for branch in set(repo.branches) - currently_created_branches:
-            sh(f"git checkout -b {branch}", cwd=repo_path)
-        sh(f"git checkout main", cwd=repo_path)
+                sh("git add .")
+                sh('git commit -m "Added submodules"')
 
 
-def extract_and_append_subrepos(repos):
-    for repo in repos[:]:
-        if "subrepos" in repo:
-            for subrepo in repo.subrepos:
-                if any(r.name == subrepo.name for r in repos):
-                    break
-                repos.append(Box({"name": subrepo.name}))
-    return repos
-
-
-@app.command()
-def generate(file: str):
-    file_path = os.path.abspath(file)
-    base_path = os.path.dirname(file_path)
-
-    data = Box.from_toml(filename=file)
-    repos = [r for r in data.repo if "name" in r]
-    print([r.name for r in repos])
-    repos = extract_and_append_subrepos(repos)
-    print([r.name for r in repos])
-    pattern_definitions = [
-        pattern_repo for pattern_repo in data.repo if "pattern" in pattern_repo
-    ]
-    sorted_patterns = sort_patterns(pattern_definitions)
-    repos = [apply_defaults_to_repo(repo, sorted_patterns) for repo in repos]
-    graph = build_dependency_graph(repos)
-
-    os.chdir(base_path)
-
-    for repo_name in nx.topological_sort(graph):
-        repo = next((r for r in repos if r.name == repo_name), None)
-        if repo:
-            create_or_update_repo(repos, repo, base_path)
-
-    for repo_name in nx.topological_sort(graph):
-        repo = next((r for r in repos if r.name == repo_name), None)
-        if repo and 'submodules' in repo:
-            ...
-        
-
-    # Displaying the tree (optional, for visualization)
-    tree = Tree("🌌 Repositories")
-    for repo in repos:
-        repo_node = tree.add(
-            f"[bold green]{repo.name}[/bold green]: {repo.description.strip() if 'description' in repo else ''}"
-        )
-        if "branches" in repo:
-            branches_node = repo_node.add("[bold yellow]Branches[/bold yellow]")
-            for branch in repo.branches:
-                branches_node.add(branch)
-        if "subrepos" in repo:
-            subrepos_node = repo_node.add("[bold blue]Subrepos[/bold blue]")
-            for subrepo in repo.subrepos:
-                subrepos_node.add(f"{subrepo.path} - {subrepo.name}")
-    console.print(tree)
+def main(toml_file: str):
+    """Main function to set up repositories and submodules from a TOML configuration file."""
+    data = Box.from_toml(filename=toml_file)
+    with cwd(Path(toml_file).parent):
+        repos = identify_repos(data)
+        patterns = identify_patterns(data)
+        prep_repos(repos, patterns)
+        create_or_clone_repos(repos)
+        setup_submodules(repos)
+        console.print("[green]Repository setup completed successfully![/green]")
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
